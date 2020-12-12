17834f0ce567a9ff50fd23941fbadd8ff5da49e2
qiuwenwu <573242395@qq.com>
2020-12-12 08:13:22 +0800

    更新

--- demo/app/home/plugin/pc/api_home_pc/root/index.js
+++ demo/app/home/plugin/pc/api_home_pc/root/index.js
@@ -1,4 +1,5 @@
-const file = '../../static/index.html'.fullname(__dirname);
+const tpl_dir = "../../static/".fullname(__dirname);
+const tpl_file = tpl_dir + '{0}.html';
 
 /**
  * @description 接口主函数
@@ -9,6 +10,18 @@ const file = '../../static/index.html'.fullname(__dirname);
 async function main(ctx, db) {
 	var path = ctx.path;
 	if (path.indexOf('.') === -1) {
+
+		// 抽取文件名
+		var name = path.replace("/", "");
+		if (!name) {
+			name = "index";
+		}
+		var file = tpl_file.replace("{0}", name);
+		if (!file.hasFile()) {
+			return;
+		}
+
+		// 获取当前的语言包
 		var type = ctx.cookies.get("lang_type") || 'en';
 		db.table = "sys_lang";
 		db.size = 0;
@@ -20,31 +33,62 @@ async function main(ctx, db) {
 				lang[o.key] = o[type];
 			}
 		}
-		
-		$.globalBag.congfig = $.config;
+
+		// 获取seo配置
+		db.table = "sys_seo";
+		var seo = await db.getObj({
+			path
+		});
+		if (!seo) {
+			seo = {};
+		}
+
+		// 获取系统配置
+		db.table = "sys_config";
+		var config = await db.get();
+		if (!config) {
+			config = {};
+		}
+
+
+		var query = Object.assign({}, ctx.request.query);
+		var list = [];
+		var obj = {};
+		if (name.indexOf("/list") !== -1 || name.indexOf("/channel") !== -1 || name.indexOf("/type") !== -1) {
+			// 获取系统配置
+			db.table = ("cms_" + name).replace("/list", "").replaceAll("/", "_");
+			db.page = query.page || 1;
+			db.size = query.size || 10;
+			delete query.page;
+			delete query.size;
+			list = await db.get(query);
+		} else if (name.indexOf("/view") !== -1 || name.indexOf("/form") !== -1) {
+			// 获取系统配置
+			db.table = ("cms_" + name).replace("/view", "").replace("/form", "").replaceAll("/", "_");
+			obj = await db.getObj(query);
+		}
+
+		// 创建与view视图通讯的模型
 		var model = {
 			os: "mm",
 			app: "home",
 			plugin: "pc",
 			site_name: "elins.cn",
-			title: $.config.sys.title + "门户",
-			keywords: "mm home pc",
-			description: "",
-			content: "",
-			list: [
-				{ 
-					name: "张三",
-					age: 21,
-					sex: 1
-				},
-				{
-					name: "李四",
-					age: 31,
-					sex: 0
-				}
-			],
-			lang
+			congfig: Object.assign(config, $.config),
+			seo: Object.assign({
+				title: $.config.sys.title + "门户",
+				keywords: "mm home pc",
+				description: "",
+				content: "",
+			}, seo),
+			list,
+			obj,
+			lang,
+			view: function(file, m){
+				return db.tpl.view(file.fullname(tpl_dir), Object.assign({}, model, m));
+			}
 		};
+
 		// return model;
 		// art模板引擎
 		return db.tpl.view(file, model);
